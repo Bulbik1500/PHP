a9a0fb7b30fbfcb064cb1c3eb9f8d088ee6068c1
bulbik1500 <bulbik1500@gmail.com>
2022-12-25 21:22:38 +0100

    Vowel count passed

--- Vowel Count.php
+++ Vowel Count.php
@@ -1,7 +1,29 @@
 <?php
-$str = "abracadabra";
-$unique = array();
-array_push($unique, $str[0]);
-for($i = 0; $i < (strlen($str) - 1);$i++){
-
+$str = "o a kak a lil vo kashu kakao";
+function getCount($str)
+{
+    $return_value = 0;
+    for ($i = 0; $i < strlen($str); $i++) {
+        $x = $str[$i];
+        // a e i o u 
+        switch ($x) {
+            case "a":
+                $return_value++;
+                break;
+            case "e":
+                $return_value++;
+                break;
+            case "i":
+                $return_value++;
+                break;
+            case "o":
+                $return_value++;
+                break;
+            case "u":
+                $return_value++;
+                break;
+        }
+    }
+    return $return_value;
 }
+print(getCount($str));
